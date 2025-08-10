74bf1ea2ebd50456bcb7fba4046d986fd005fd8b
SHIRT <shirt@el-dokan.com>
2025-08-10 17:02:13 +0300

    fix all server types

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 // /api/index.js
 import 'dotenv/config';
 import { mcpServer } from '../src/mcp-server.js';
-import { createSSEServer } from '../src/server.js';
+import { createSSEServer } from '../src/sse-server.js';
 
 // Always export Express app for hosting environment
 const sseServer = createSSEServer(mcpServer);
